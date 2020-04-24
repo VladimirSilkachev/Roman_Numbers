4f12462a255f5782fcf76bcbfed9231ffd2299f6
VladimirSilkachev <55372973+VladimirSilkachev@users.noreply.github.com>
2020-04-24 14:25:49 +0700

    Final

--- Rim.py
+++ Rim.py
@@ -14,14 +14,14 @@ class Rim:
             for lt2 in rim_number[ltn + 1:]:
                 if lt1 == lt2:
                     cnt += 1
-            if cnt >= 4:
+            if cnt >= 4:                            # Проверка на повторы знаков.
                 self.exception = False
                 break
 
         if self.exception:
             for i in self.rim_number:
-                if i not in all_num:
-                    self.exception = False
+                if i not in all_num:                # Проверка на корректные символы.
+                    self.exception = False      
                     break
 
     def __str__(self):
@@ -32,15 +32,15 @@ class Rim:
             ten = 0
 
             for i in self.rim_number:
-                dict_ten.append(dict_num[i])
+                dict_ten.append(int(dict_num[i]))    # Представление римского числа в виде списка из арабских цифр.
             for j in range(len(dict_ten)):
                 try:
-                    if dict_ten[j] < dict_ten[j + 1]:
-                        ten -= int(dict_ten[j])
+                    if dict_ten[j] < dict_ten[j + 1]:  
+                        ten -= dict_ten[j]           # Нужно отнять, если сначала идет меньшее число, а затем большее.
                     else:
-                        ten += int(dict_ten[j])
+                        ten += dict_ten[j]           # И прибавить если наоборот.
                 except IndexError:
-                    ten += int(dict_ten[j])
+                    ten += dict_ten[j]               # Последнее число прибавляется, исключение на случай выхода из диапазона.
 
             return str(ten)
 
